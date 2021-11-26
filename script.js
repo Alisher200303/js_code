89e1262198c7c7c5e9e978087f11614cdcc47c73
Web_design <alishertogoyev682@gmail.com>
2021-11-26 05:41:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,179 @@
+"use strict";
+/**Lesson_10 */
+
+
+
+
+
+
+
+
+
+
+
+/**Lesson_9 */
+/*
+const x = 10,
+y = 15;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x / y);
+console.log(x * y);
+
+console.log('Hello' + " " + 'World');
+
+const num = 4 + 'radio';
+
+console.log(typeof num);
+console.log(4 + 'radio');
+
+let z = 10;
+console.log(++z);
+console.log(z);
+
+let incer = 99;
+let decr = 99;
+
+// console.log(++incer);
+// console.log(--decr);
+
+console.log(incer++);
+console.log(decr--);
+
+console.log(incer);
+console.log(decr);
+
+
+console.log(5 % 2);
+
+const q = 15;
+const m = 7;
+
+console.log(q % m);
+
+
+
+
+
+console.log(2 * 3 == '6');
+console.log(2 * 3 === "6");
+
+
+const isChecked = true;
+ isClose = false;
+
+console.log(isChecked && (isClose || !isChecked));
+
+*/
+
+
+
+
+
+/**Lesson_8 */
+/**
+const firstName = 'Alisher';
+const age = 18;
+
+console.log(`My name is ${firstName} va yoshim ${age}`);
+const xName = prompt("Enter your name", "");
+console.log(`Users name ${xName}`);
+
+*/
+
+/**Lesson_7 */
+// console.log('Hello World');
+
+// alert('Hello world');
+
+/** 
+const user = confirm('Siz uylanganmisiz');
+console.log(user);
+*/
+
+/*
+const userName = prompt('Ismoz nima', '');
+console.log(userName);
+*/
+
+
+// const answers = [];
+// answers[0] = prompt('Ism kiriting');
+// answers[1] = prompt('Familiya kiriting');
+// answers[2] = prompt('Yoshiz nechida');
+
+// console.log(answers);
+
+// const firstName = +prompt('Yoshiz nima');
+// console.log(firstName + 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Lesson_2 /*'
+// use strict
+// const number = 8; // data-type = number
+
+// console.log(-4 / 0);
+// console.log('alisher' * 10); // Not a number
+// const firstName = 'alisher'; // data-type = string
+// const boolean = true; // data-type = boolean
+
+// const isMarried = false;
+
+// /** console.log(a); // data-type = null */
+
+// let = lastName;
+// console.log(lastName);
+
+// const person = {
+//     firstName: 'alisher',
+//     lastName: 'togoyev',
+//     age: 18,
+//     job: "Cyber Security",
+//     isMarried: false,
+// }
+// console.log(person.firstName);
+
+// const arr = ['red', 'black', 'yellow'];
+// console.log(arr[0]); *
+// *
+
+/** Lesson_1 **/
+
+/*
+"use strict";
+
+
+a = 15;
+console.log(a);
+let firstName = 3;
+console.log(firstName);
+
+firstName = 5;
+console.log(firstName);
+
+const pi = 3.14;
+pi = 4;
+console.log(pi);
+var x = 15;
+console.log(x); {
+    let y = 15;
+}
+console.log(y);
+console.log(y);
+*/
